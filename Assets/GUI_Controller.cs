490b62d9f490260e8a1a0af9e51b040ffb8971be
AlessioBona <alefbona@gmail.com>
2020-03-28 20:22:35 +0100

    Web adjusts automatically

--- Assets/GUI_Controller.cs
+++ Assets/GUI_Controller.cs
@@ -8,7 +8,9 @@ public class GUI_Controller : MonoBehaviour
     public Image HouseFill;
     public Image BarFill;
     public Image WebColor;
+    public Transform WebPosition;
     public Animator myAnimator;
+    public float barTreshold = 5f;
 
 
     // Start is called before the first frame update
@@ -16,8 +18,9 @@ public class GUI_Controller : MonoBehaviour
     {
         myAnimator = GetComponent<Animator>();
         HouseFill.fillAmount = 0;
-        BarFill.fillAmount = 1;
+        //SetEnergyValue(1);
         WebColor.gameObject.SetActive(false);
+        WebPosition.localPosition = new Vector3(WebPosition.localPosition.x, -755 + ( (755 + 150) * barTreshold), 0);
     }
 
     public void SetCompletionValue(float value)
@@ -25,9 +28,21 @@ public class GUI_Controller : MonoBehaviour
         HouseFill.fillAmount = value;
     }
 
+    //public void Update()
+    //{
+    //    SetEnergyValue(BarFill.fillAmount - 0.001f);
+    //}
+
     public void SetEnergyValue(float value)
     {
         BarFill.fillAmount = value;
+        if(value >= barTreshold)
+        {
+            EnableDisableWeb(true);
+        } else
+        {
+            EnableDisableWeb(false);
+        }
     }
 
     public void EnableDisableWeb(bool value)

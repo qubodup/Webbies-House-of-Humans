5b9ee7fbaeccfef76bc35434d2a8cd0585f70bac
AlessioBona <alefbona@gmail.com>
2020-03-28 19:08:06 +0100

    GUI prefab ready

--- Assets/GUI_Controller.cs
+++ Assets/GUI_Controller.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GUI_Controller : MonoBehaviour
 {
+    public Image HouseFill;
+    public Image BarFill;
+    public Image WebColor;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HouseFill.fillAmount = 0;
+        BarFill.fillAmount = 1;
+        WebColor.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetCompletionValue(float value)
     {
-        
+        HouseFill.fillAmount = value;
     }
+
+    public void SetEnergyValue(float value)
+    {
+        BarFill.fillAmount = value;
+    }
+
+    public void EnableDisableWeb(bool value)
+    {
+        WebColor.enabled = value;
+    }
+
+    public void NegativeFeedback()
+    {
+
+    }
+        
 }
